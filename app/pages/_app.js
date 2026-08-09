50e631ec373adc161b9ee8326dcf5c46e106697f
Kriti yadav <156906180+Kride024@users.noreply.github.com>
2026-08-09 20:12:58 +0530

    Update _app.js

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,14 +1,5 @@
-import { NhostProvider } from "@nhost/nextjs";
-import { NhostApolloProvider } from "@nhost/react-apollo";
-import { nhost } from "../lib/nhost";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <NhostProvider nhost={nhost} initial={pageProps.nhostSession}>
-      <NhostApolloProvider nhost={nhost}>
-        <Component {...pageProps} />
-      </NhostApolloProvider>
-    </NhostProvider>
-  );
+  return <Component {...pageProps} />;
 }
